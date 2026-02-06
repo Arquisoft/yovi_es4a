7e83a4f752f5cc028e93ded209823266b0ee4cf3
Marcelo <marcelodiezdominguez@gmail.com>
2026-02-06 11:47:20 +0100

    Creación Interfaz básica + Conexión Back-end y Front-end

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,39 +1,50 @@
 import { useState } from 'react';
-import './App.css'
-import yoviLogo from './assets/yoviLogo2.svg'
+import './App.css';
+import yoviLogo from './assets/yoviLogo2.svg';
+
 import RegisterForm from './RegisterForm';
 import Home from './Home';
-import GameBoard from './GameBoard';
+import Board from './Board';
+
+type View = 'register' | 'home' | 'board';
+
 function App() {
   const [username, setUsername] = useState<string | null>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [view, setView] = useState<View>('register');
 
   const handleUserRegistered = (registeredUsername: string) => {
     setUsername(registeredUsername);
+    setView('home');
   };
 
   const handleStartGame = () => {
-    setIsPlaying(true);
+    setView('board');
+  };
+
+  const handleExitBoard = () => {
+    setView('home');
   };
 
   return (
     <div className="App">
-      {username ? (
-        isPlaying ? (
-          <GameBoard username={username} size={7} onExit={() => setIsPlaying(false)} />
-        ) : (
-          <Home username={username} onPlay={handleStartGame} />
-        )
-      ) : (
-        <>
-          <div>
-            <a href="https://github.com/Arquisoft/yovi_es4a" target="_blank" rel="noreferrer">
-              <img src={yoviLogo} className="logo yovi" alt="YOVI logo" />
-            </a>
-          </div>
-          <h1>Welcome to YOVI</h1>
-          <RegisterForm onUserRegistered={handleUserRegistered} />
-        </>
+      <div>
+        <a href="https://github.com/Arquisoft/yovi_es4a" target="_blank" rel="noreferrer">
+          <img src={yoviLogo} className="logo yovi" alt="YOVI logo" />
+        </a>
+      </div>
+
+      <h1>Welcome to YOVI</h1>
+
+      {view === 'register' && (
+        <RegisterForm onUserRegistered={handleUserRegistered} />
+      )}
+
+      {view === 'home' && username && (
+        <Home username={username} onPlay={handleStartGame} />
+      )}
+
+      {view === 'board' && username && (
+        <Board username={username} size={7} onExit={handleExitBoard} />
       )}
     </div>
   );
--- webapp/src/Home.tsx
+++ webapp/src/Home.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 interface HomeProps {
   username: string;
   onPlay: () => void;
