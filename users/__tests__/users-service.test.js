7405b21c1b8ad85c45984408d6bf51272fc099a7
uo302457 <uo302457@uniovi.es>
2026-02-28 18:05:48 +0100

    Test modificado para su correcto funcionamiento

--- users/__tests__/users-service.test.js
+++ users/__tests__/users-service.test.js
@@ -1,14 +1,23 @@
 import { describe, it, expect, afterEach, vi } from 'vitest'
 import request from 'supertest'
-import app from '../users-service.js'
 
-vi.mock('../user-model.js', () => {
+vi.mock('mongoose', async () => {
+    const actual = await vi.importActual('mongoose')
+    return {
+        ...actual,
+        connect: vi.fn().mockResolvedValue(true)
+    }
+})
+
+vi.mock('../users-model.js', () => {
     const User = vi.fn().mockImplementation(() => ({
         save: vi.fn().mockResolvedValue(true)
     }));
     User.findOne = vi.fn();
     return { default: User };
-});
+})
+
+import app from '../users-service.js'
 
 describe('POST /createuser', () => {
     afterEach(() => {
@@ -18,11 +27,11 @@ describe('POST /createuser', () => {
     it('returns a greeting message for the provided username', async () => {
         const res = await request(app)
             .post('/createuser')
-            .send({ username: 'Test5', password: '1234' })
+            .send({ username: 'Test1234', password: '1234' })
             .set('Accept', 'application/json')
 
         expect(res.status).toBe(200)
         expect(res.body).toHaveProperty('message')
-        expect(res.body.message).toMatch(/Bienvenido Test5/i)
+        expect(res.body.message).toMatch(/Bienvenido Test1234/i)
     })
 })
